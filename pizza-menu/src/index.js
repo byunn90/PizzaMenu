6b1b9450cd52f532d7d6cd5fab13f8ee2d045dea
byunn90 <kayhanturk90@Live.com>
2023-10-14 19:31:22 +1100

    react fragments

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -84,14 +84,21 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic italian cuisine. 6 creactive dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
-        <p>We're still working on our menu. Please come back later 😆</p>
+        <p>We're still working on our menu. Please come back later 😊</p>
       )}
 
       {/* <Pizza
